94866008080975f2dda4dd50a249325349a827ea
rogetgeoffroy26 <rogetgeoffroy@gmail.com>
2018-10-18 21:00:27 -0400

    React API Test

---
+++ api-app/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  constructor(props) {
+  	super(props)
+  	this.state = {
+  		items: [],
+  		isLoaded: false,
+  	}
+  }
+
+  componentDidMount(){
+  	fetch('http://jsonplaceholder.typicode.com/users')
+  	.then(res => res.json())
+  	.then(json => {
+  		this.setState({
+  			isLoaded: true,
+  			items: json,
+  		})
+  	});
+  }
+
+  render(){
+    var { isLoaded, items } = this.state;
+    if(!isLoaded){
+      return <div> Loading… </div>
+    }
+    else{
+
+      return (
+        <div className="App">
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                Name: {item.name} | Email: {item.email}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
